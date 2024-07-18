96127bea4890522f17d64009fb7179ae035fa5e8
anasachu2000 <muhammedanasea2000@gmail.com>
2024-07-18 17:03:46 +0530

    update: implimented

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server');
+const cron = require('node-cron');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -9,4 +11,11 @@ server.use(router);
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('JSON Server is running on port', port);
+});
+
+// Schedule a cron job to run every minute
+cron.schedule('* * * * *', () => {
+  console.log('Cron job running every minute');
+  // Add your cron job task here
+  // For example, you could read/write to the database, make an API call, etc.
 });
